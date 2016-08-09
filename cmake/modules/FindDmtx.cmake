758a9011e92e9ad48034af96916a96adbb9601ee
Christophe Giboudeaux <cgiboudeaux@gmx.com>
2016-08-09 21:25:39 +0200

    Clean FindDmtx.cmake
    Since 202413b, CMake was failing when trying to find libdmtx a second time.

--- cmake/modules/FindDmtx.cmake
+++ cmake/modules/FindDmtx.cmake
@@ -1,30 +1,81 @@
-# - Try to find the dmtx library
-# Once done this will define
+#.rst:
+# FindDmtx
+# --------
 #
-#  DMTX_FOUND - system has the datamatrix library
-#  DMTX_INCLUDE_DIR - the datamatrix library include dir
-#  DMTX_LIBRARIES - the libraries used to link datamatrix
-
+# Try to find the DataMatrix library.
+#
+# This will define the following variables:
+#
+# ``Dmtx_FOUND``
+#     True if the datamatrix library is available.
+#
+# ``Dmtx_INCLUDE_DIRS``
+#     the datamatrix library include dirs.
+#     This variable shall be passed to target_include_libraries() calls if
+#     the target is not used for linking.
+#
+# ``Dmtx_LIBRARIES``
+#     the libraries used to link datamatrix.
+#     This can be passed to target_link_libraries instead of
+#     the ``Dmtx::Dmtx`` target.
+#
+# If ``Dmtx_FOUND`` is TRUE, the following imported target
+# will be available:
+#
+# ``Dmtx::Dmtx``
+#     The datamatrix library.
+#
+# Imported target since 1.2.2
+#
+#=============================================================================
 # Copyright (C) 2010 Sune Vuorela <sune@debian.org>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#=============================================================================
 
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+find_path(Dmtx_INCLUDE_DIRS dmtx.h)
 
-if (DMTX_INCLUDE_DIR AND DMTX_LIBRARIES)
-  # in cache already
-  set(DTMX_FOUND TRUE)
-else (DMTX_INCLUDE_DIR AND DMTX_LIBRARIES)
+find_library(Dmtx_LIBRARIES NAMES dmtx)
 
-  find_path(DMTX_INCLUDE_DIR dmtx.h)
+include(FindPackageHandleStandardArgs)
 
-  find_library(DMTX_LIBRARIES NAMES dmtx)
+find_package_handle_standard_args(Dmtx
+    REQUIRED_VARS Dmtx_INCLUDE_DIRS Dmtx_LIBRARIES
+)
 
-  if (DMTX_INCLUDE_DIR AND DMTX_LIBRARIES)
-     set(DMTX_FOUND TRUE)
-  else (DMTX_INCLUDE_DIR AND DMTX_LIBRARIES)
-     message("Datamatrix library not found, please see http://www.libdmtx.org")
-  endif (DMTX_INCLUDE_DIR AND DMTX_LIBRARIES)
+if(Dmtx_FOUND AND NOT TARGET Dmtx::Dmtx)
+  add_library(Dmtx::Dmtx UNKNOWN IMPORTED)
+  set_target_properties(Dmtx::Dmtx PROPERTIES
+  IMPORTED_LOCATION "${Dmtx_LIBRARIES}"
+  INTERFACE_INCLUDE_DIRECTORIES "${Dmtx_INCLUDE_DIRS}")
+endif()
 
-  mark_as_advanced(DMTX_INCLUDE_DIR DMTX_LIBRARIES)
+mark_as_advanced(Dmtx_INCLUDE_DIRS Dmtx_LIBRARIES)
 
-endif (DMTX_INCLUDE_DIR AND DMTX_LIBRARIES)
+include(FeatureSummary)
+set_package_properties(Dmtx PROPERTIES
+  URL "http://libdmtx.sourceforge.net/"
+  DESCRIPTION "The Datamatrix library."
+)
+

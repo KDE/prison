3a0aa5897182852214ac692ef0e5d6ee11d9a7b6
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2020-02-01 19:13:32 +0100

    Prison: Convert license statements to SPDX identifiers
    Summary:
    Update all existing license and copyright statements
    to SPDX identifiers.
    
    Reviewers: cgiboudeaux
    
    Reviewed By: cgiboudeaux
    
    Subscribers: cgiboudeaux, kde-frameworks-devel
    
    Tags: #frameworks
    
    Maniphest Tasks: T11550
    
    Differential Revision: https://phabricator.kde.org/D27092

--- src/tools/prison-datamatrix.cpp
+++ src/tools/prison-datamatrix.cpp
@@ -1,27 +1,7 @@
 /*
-    Copyright (c) 2010-2016 Sune Vuorela <sune@vuorela.dk>
+    SPDX-FileCopyrightText: 2010-2016 Sune Vuorela <sune@vuorela.dk>
 
-    Permission is hereby granted, free of charge, to any person
-    obtaining a copy of this software and associated documentation
-    files (the "Software"), to deal in the Software without
-    restriction, including without limitation the rights to use,
-    copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the
-    Software is furnished to do so, subject to the following
-    conditions:
-
-    The above copyright notice and this permission notice shall be
-    included in all copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-    OTHER DEALINGS IN THE SOFTWARE.
-
+    SPDX-License-Identifier: MIT
 */
 
 #include <QCoreApplication>
@@ -74,7 +54,7 @@ int main(int argc, char* argv[]) {
   if(!QImageWriter::supportedImageFormats().contains(outputformat.toLocal8Bit())) {
     error(QStringLiteral("unsupported output format"), outputformat);
   }
-  
+
   if(outputfile.isEmpty()) {
     error(QStringLiteral("outputfile is missing"),QString());
   }
@@ -88,8 +68,8 @@ int main(int argc, char* argv[]) {
     error(QStringLiteral("needs a larger output size"),size);
   }
 
-  
-  
+
+
   QString data = arguments.join(QLatin1Char(' '));
   if(data.size()==0) {
     QTextStream in(stdin);
@@ -111,5 +91,5 @@ int main(int argc, char* argv[]) {
   if(!w.write(result)) {
     error(QStringLiteral("writing failed"),w.errorString());
   }
-  
+
 }

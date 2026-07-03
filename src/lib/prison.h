a214f6b49268e99e1f457379f86b1d5f27bb3494
Nicolas Fella <nicolas.fella@gmx.de>
2026-07-03 12:46:54 +0200

    Fix documentation syntax

--- src/lib/prison.h
+++ src/lib/prison.h
@@ -29,8 +29,8 @@ namespace Prison
  * \value Code128 Code 128 barcode
  * \value PDF417 PDF417 barcode
  * \value EAN13 EAN13 barcode
- * \value ITF Industrial 2 of 5 barcode (since 6.28)
- * \value Codabar Codabar barcode (since 6.28)
+ * \value [since 6.28] ITF Industrial 2 of 5 barcode
+ * \value [since 6.28] Codabar Codabar barcode
  */
 enum BarcodeType {
     QRCode,
